f815e868dfa3ed7fc3b9e9d6a0c50148fab33640
Ashton A <abouanasht@ecs.vuw.ac.nz>
2019-04-30 14:34:33 +1200

    fix issue loading empty files
    Trying to load the list of tickers
    from an empty file with pickle
    triggered EOF error. After fix program
    now returns an empty list if file
    is empty. Also created function
    that prints contents of files
    that have had ticker lists stored
    written to them.

--- venv/src/tickerFile.py
+++ venv/src/tickerFile.py
@@ -6,47 +6,72 @@
 import pickle as pk
 
 class TickerFileWriter:
-    filename
+
 
     def __init__(self, filename):
         self.filename = filename
 
     #add all tickers in file to list
-    def appendTickers(tickers):
-        with open(filename, 'wb+rb') as fp:
-            tickerList = pk.load(fp)
-            for ticker in tickers:
-                if ticker not in tickerList:
-                    tickerList.append(ticker)
+    def appendTickers(self,tickers):
+        tickerList = self.readListFromFile()
+        for ticker in tickers:
+            if ticker not in tickerList:
+                tickerList.append(ticker)
+
+        fp = open(self.filename, "wb")
         pk.dump(tickerList,fp)
-        close(filename)
+        fp.close()
 
     #remove all tickers in tickers from file
-    def removeTickers(tickers):
-        tickerList = loadListFromFile(filename)
-        with open(filename, 'wb+rb') as fp:
-            tickerList = pk.load(fp)
-            for ticker in tickers
-                tickerList.remove(ticker)
+    def removeTickers(self,tickers):
+        tickerList = self.readListFromFile()
+        for ticker in tickers:
+            tickerList.remove(ticker)
+        fp = open(self.filename, "wb")
         pk.dump(tickerList, fp)
-        close(fp)
+        fp.close()
 
     #update function. Adds all tickers in list to file and removes all tickers not found in list.
-    def updateAllTickers(tickers):
-        with open(filename, 'wb+rb') as fp:
-            tickerList = pk.load(fp)
-            for ticker in tickers:
-                if ticker not in tickerList:
-                    tickerList.append(ticker)
-            for ticker in tickerList:
-                if ticker not in tickers:
-                    tickerList.remove(ticker)
+    def updateAllTickers(self,tickers):
+        tickerList = self.readListFromFile()
+        removeList = []
+        for ticker in tickers:
+            if ticker not in tickerList:
+                tickerList.append(ticker)
+        for ticker in tickerList:
+            if ticker not in tickers:
+                removeList.append(ticker)
+        for ticker in removeList:
+            tickerList.remove(ticker)
+        fp = open(self.filename, "wb")
         pk.dump(tickerList, fp)
-        close(fp)
+        fp.close()
 
+    def readListFromFile(self):
+        try:
+            fp = open(self.filename, "rb+")
+            tickerList = pk.load(fp)
+            fp.close()
+        except EOFError:
+            tickerList = []
+        return tickerList
+
+    def printTickerFile(self):
+        try:
+            with open(self.filename, 'rb') as fp:
+                print(pk.load(fp))
+        except EOFError:
+            print("File Empty")
 
     def setFilename(filename):
         self.filename = filename
 
     def getFilename():
         return self.filename
+
+tickerL = ["ASX", "NKE", "NVD", "LMO"]
+tk = TickerFileWriter("../predictions/top_performers")
+tk.appendTickers(tickerL)
+tk.printTickerFile()
+tk.removeTickers(["ASX", "BDE"])
+tk.printTickerFile()
