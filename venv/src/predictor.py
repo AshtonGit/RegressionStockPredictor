74ca5641e83680388c941020328648bda91cb44b
Ashton A <abouanasht@ecs.vuw.ac.nz>
2020-05-20 22:54:57 +1200

    Add command line user interface

--- venv/src/predictor.py
+++ venv/src/predictor.py
@@ -51,7 +51,6 @@ def predictStockLinearRegression(stock, numDays, start = None, end = None):
     clf = LinearRegression()
     clf.fit(x_train, y_train)
     score = clf.score(x_test,y_test)
-    print("Score: "+str(score))
     prediction = clf.predict(X_lately)
     return prediction
 
@@ -65,10 +64,34 @@ def getAccuracy(prediction, y_true, predictedCol,numDays):
     print("accuracy: "+str(acc))
 
 def main():
-    stockData = getStockPriceHistoryIEX("NKE", datetime.date(2018,1,1), datetime.date(2019, 1, 30))
-    prediction = predictStockLinearRegression("NKE",5,start = datetime.date(2018,1,1),
-                                   end = datetime.date(2019,1,20))
-    print("Linear Regression: ")
-    getAccuracy(prediction, stockData, 'close', 5)
+    print('Welcome to my regression stock price predictor')
+    print('First enter the 3 letter id for the stock you want predicted \n\t eg "NKE')
+    print('Then enter the date from which stock analysis will start from \n\t eg 01 05 2018')
+    print('Lastly enter how many days into the future you want the prediction to extend \n\t eg 5')
+    today = datetime.date.today()
+    while True:
+        ticker = input('\n\nStock ticker:').strip().upper()
+        if len(ticker) != 3:
+            print('Sorry, stock ticker must be exactly 3 characters long')
+            continue
+
+        s = input("Start date (dd mm yyyy):").split()
+        try:
+            start = datetime.date(int(s[2]), int(s[1]), int(s[0]))
+        except ValueError:
+            print('Sorry, invalid date given')
+            continue
+
+        days_in = input("Prediction length (days): ")
+        try:
+            numDays = int(days_in)
+        except ValueError:
+            print("Input must be a whole number")
+            continue
+
+        prediction = predictStockLinearRegression(ticker,numDays, start, today)
+        print('Predicted',ticker,'stock price over the coming',str(numDays),'days:')
+        print(prediction)
+
 
 main()
